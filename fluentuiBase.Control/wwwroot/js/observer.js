390643b2209bc147b5c45283ac2001c1d5e93f5c
tinycodecraft <raymond.cw.yau@gmail.com>
2025-05-07 15:30:28 +0800

    improve the infinite scroll logic

--- fluentuiBase.Control/wwwroot/js/observer.js
+++ fluentuiBase.Control/wwwroot/js/observer.js
@@ -1,10 +1,16 @@
-export function initObserver(component, observerTargetId) {
-    var observer = new IntersectionObserver(e => {
-        component.invokeMethodAsync('OnIntersection');
-    });
+export function initObserver(component, observerTargetId,observerContainerId) {
     let element = document.getElementById(observerTargetId);
+    let container = document.getElementById(observerContainerId);
+
+    var observer = new IntersectionObserver(e => {
+        console.log(JSON.stringify(e));
+        component.invokeMethodAsync('OnIntersection');
+    }, { root: container, rootMargin: '0px', threshold: 0.5 });
+    
     if (element == null) throw new Error("The observable target was not found");
+
     observer.observe(element);
+
     window.Observer = observer;
     return observer;
 
